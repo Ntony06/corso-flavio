43998fa0e5238efcb104b8d3d92bf746c088e2e5
Antonio <ntony_06@live.it>
2021-02-10 19:08:42 +0100

    add first functions of js

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+let balance = 0;
+const operations = [];
+const OperationsType = {
+    EXPENSE: 'EXPENSE',
+    INCOME: 'INCOME',
+}
+/**
+ * {
+ *   type: 'EXPENSE',
+ *   amount: 120,
+ *   description: 'Bill payment'
+ * }
+ * @param {*} operation 
+ */
+function addOperation(operation) {
+    const operationToAdd = operation;
+    operationToAdd.id = new Date().getTime();
+    if(operationToAdd.type === OperationsType.EXPENSE) {
+        balance = balance - operationToAdd.amount;
+    } else if(operationToAdd.type === OperationsType.INCOME) {
+        balance = balance + operationToAdd.amount;
+    }
+    operations.push(operationToAdd);
+}
+function removeOperation() {
+    operations.slice()
+}
+// Find operation
+// Get balance
+// Get operations (lista operazioni)
+function NewBalance(){
+    return(balance);
+};
