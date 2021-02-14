bb634249fc8677af057bd4f18eff7da07d7074cd
Antonio <ntony_06@live.it>
2021-02-14 20:46:19 +0100

    add validation for find, add jsdoc

--- src/js/index.js
+++ src/js/index.js
@@ -4,38 +4,42 @@
     let balance = 0;
     const operations = [];
     const WalletErrors = {
-        OPERATION_NOT_FOUND: 'OPERATION_NOT_FOUND',
-        INPUT_INVALID: "iNPUT_INVALID",
-        INVALID_TYPE: "INVALID_TYPE",
-        INVALID_AMOUNT: "INVALID_AMOUNT",
-        INVALID_DESCRPT: "INVALID_DESCRIPTION"
+        NOT_FOUND: 'OPERATION_NOT_FOUND',
+        TYPE: "INVALID_TYPE",
+        AMOUNT: "INVALID_AMOUNT",
+        DESCRPT: "INVALID_DESCRIPTION",
+        KEY: "INVALID_KEYWORD"
     }
 
     
-const OperationsType = {
-    EXPENSE: 'EXPENSE',
-    INCOME: 'INCOME'
-}
+        const OperationsType = {
+            EXPENSE: 'EXPENSE',
+            INCOME: 'INCOME'
+        }
 
     /**
-     * {
+     * this function add a operation from the wallet.
+     * es {
      *   type: 'EXPENSE',
      *   amount: 120,
      *   description: 'Bill payment'
      * }
      * @param {*} operation 
+     * @constructor ????
      */
     this.addOperation = function(operation) {
 
         // EXTRA, FIXATO VALIDATORE
-        // 1. Estrarre questa validazione in un'altra funzione che prenda come parametro l'operazione e restituisca true o false
+        // 1. Estrarre questa validazione in un'altra funzione che prenda come parametro l'operazione e restituisca true o false-------------- ok
         // 2. Personalizzare l'errore ----------------------- ok 
-        // 3. Aggiungere validazione in altre funzioni che reputi opportune
-        // 4. Esporta correttamente le funzioni nel contesto padre   ----------------------- ok
-        // 5. aggiungere documentazione alle funzioni con commento (es jsdoc)
-        if(!OperationsType[operation.type] || operation.amount <= 0 || !operation.description) {
-            throw new Error(WalletErrors.INPUT_INVALID);
-        }
+        // 3. Esporta correttamente le funzioni nel contesto padre   ----------------------- ok
+        // 4. aggiungere documentazione alle funzioni con commento (es jsdoc) ok 
+        if(!OperationsType[operation.type])
+            { throw new Error(WalletErrors.TYPE);}
+        if( operation.amount <= 0)
+            { throw new Error(WalletErrors.AMOUNT);}
+        if(!operation.description)
+            { throw new Error(WalletErrors.DESCRPT);}
         const operationToAdd = operation;
         operationToAdd.id = new Date().getTime();
         if (operationToAdd.type === OperationsType.EXPENSE) {
@@ -49,6 +53,7 @@ const OperationsType = {
      * Remove an operation from the wallet. It receives the id of the operation
      * and then removes it from the operations list.
      * @param {number} operationId
+     * 
      */
     this.removeOperation = function (operationId) {
         let idToRemove = -1;
@@ -59,8 +64,8 @@ const OperationsType = {
             }
         }
         if (idToRemove === -1) {
-            throw new Error(WalletErrors.OPERATION_NOT_FOUND);
-        } // for new balance
+            throw new Error(WalletErrors.NOT_FOUND);
+        } 
         const operation = operations[idToRemove];
         if (operation.type === OperationsType.INCOME) {
             balance -= operation.amount;
@@ -73,9 +78,11 @@ const OperationsType = {
     /**
     * Find a list of operations which descriptions match at least partially the search value.
     * @param {string} searchValue
-    * @return {Array<Object>}
+    * @return {Array<{operation}>} 
     */
     this.findOperations = function (searchValue) {
+        if(!searchValue || typeof searchValue ==="number")
+        { throw new Error(WalletErrors.KEY);}
         const val = searchValue.toLowerCase().trim();
         const operationsFound = [];
         for (let i = 0; i < operations.length; i++) {
@@ -88,17 +95,17 @@ const OperationsType = {
     }
 
     /**
-     * @return {number} Balance of the wallet
+     * 
+     * @return {number}  return actually Balance of the wallet
      */
     this.getBalance = function() {
         return balance;
     }
     /**
-     * @return {Array<Object>} Returns the operations list of the wallet
+     * @return {Array<{operation}>} Returns the operations list of the wallet
      */
     this.getOperations = function() {
         return operations
     }
 
-    
 })();
